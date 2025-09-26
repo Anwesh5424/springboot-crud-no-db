4306368e55fc502d0bc9f10cef73a620e05af1fc
Anwesh Ammula <ammulaanwesh5424@gmail.com>
2025-09-25 21:51:07 -0500

    added quantity to product model

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -4,13 +4,23 @@ public class Product {
     private int id;
     private String name;
     private double price;
+    private int quantity;
 
     // Constructors
     public Product() {}
-    public Product(int id, String name, double price) {
+    public Product(int id, String name, double price, int quantity) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
     // Getters
